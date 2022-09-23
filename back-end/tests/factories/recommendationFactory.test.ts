60c7afa17a8329d765ae00514e8f462f790b2fdd
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-23 13:54:42 -0300

    :recycle: refactor: remake function for create data for recommendation

--- back-end/tests/factories/recommendationFactory.test.ts
+++ back-end/tests/factories/recommendationFactory.test.ts
@@ -3,21 +3,16 @@ import { Recommendation } from "@prisma/client"
 
 type FactoryRecommendation = Omit<Recommendation, "id">
 
+
+
 export const recommendationFactory = {
-  createData: (
-    name = "Test Name",
-    youtubeLink = "https://www.youtube.com/watch?v=bOhasdCq1OU",
+  createRandomData: (
+    name = faker.music.songName(),
+    youtubeLink = `https://www.youtube.com/watch?${name}`,
     score = 0
   ): FactoryRecommendation => ({
     name,
     youtubeLink,
     score,
   }),
-
-  createRandomData: (score = 0): FactoryRecommendation => ({
-    name: faker.lorem.words(3),
-    youtubeLink: "https://www.youtube.com/watch?v=bOhasdCq1OU",
-    score
-  }),
-
 }
