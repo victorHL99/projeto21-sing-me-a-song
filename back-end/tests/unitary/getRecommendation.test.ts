9f9774376f3c2113754568408d8e1f5214905ac7
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-26 14:38:22 -0300

    :white_check_mark: test: create test for return GET by id

--- back-end/tests/unitary/getRecommendation.test.ts
+++ back-end/tests/unitary/getRecommendation.test.ts
@@ -11,3 +11,23 @@ beforeEach(async () => {
   jest.resetAllMocks();
   jest.clearAllMocks();
 });
+
+describe("GET recommendationService test suite", () => {
+  describe("GET by id", () => {
+    it("Should return a recommendation by id", async () => {
+      const recommendation = {
+        id: 1,
+        name: "TesteGet",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 0
+      }
+
+      jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(recommendation as any)
+
+      const result = await recommendationService.getById(1);
+
+      expect(result).toEqual(recommendation);
+      expect(recommendationRepository.find).toHaveBeenCalledTimes(1);
+    });
+  })
+})
