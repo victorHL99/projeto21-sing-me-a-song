aba5665b2d723f0a0eb3b7d494942fc24aafdeec
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-26 13:17:54 -0300

    :white_check_mark: test: create test unit for update vote and downvote

--- back-end/tests/unitary/postRecommendation.test.ts
+++ back-end/tests/unitary/postRecommendation.test.ts
@@ -8,9 +8,10 @@ import { recommendationFactory } from '../../src/factories/recommendationFactory
 
 beforeEach(async () => {
   await prisma.$executeRaw`TRUNCATE TABLE "recommendations" `;
+  jest.clearAllMocks();
 });
 
-describe("POST recommendation test suite", () => {
+describe("Recommendation test suite", () => {
   it("Should insert new recommendation and call insert function 1 time", async () => {
     // Arrange
     const { name, youtubeLink } = recommendationFactory.createRandomData();
@@ -24,8 +25,47 @@ describe("POST recommendation test suite", () => {
     expect(recommendationRepository.create).toHaveBeenCalledTimes(1);
     expect(recommendationRepository.findByName).toHaveBeenCalledTimes(1);
   });
-
 })
+
+describe("Vote test suite", () => {
+  describe("Upvote", () => {
+    it("Should upvote a exist song recommendation", async () => {
+      jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(true as any)
+      jest.spyOn(recommendationRepository, "updateScore").mockResolvedValueOnce(null)
+
+      await recommendationService.upvote(1)
+
+      expect(recommendationRepository.find).toBeCalledTimes(1)
+      expect(recommendationRepository.updateScore).toBeCalledTimes(1)
+    });
+
+    it("Should not upvote a non exist song recommendation", async () => {
+      jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(null)
+      jest.spyOn(recommendationRepository, "updateScore").mockResolvedValue(null)
+
+      const result = recommendationService.upvote(1)
+
+      expect(result).rejects.toHaveProperty("type", "not_found")
+      expect(recommendationRepository.find).toBeCalledTimes(1)
+      expect(recommendationRepository.updateScore).not.toBeCalled()
+    })
+  });
+
+  describe("Downvote", () => {
+    it("Should downvote a exist song recommendation", async () => {
+      jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(true as any)
+      jest.spyOn(recommendationRepository, "updateScore").mockResolvedValueOnce(true as any)
+      jest.spyOn(recommendationRepository, "remove")
+
+      await recommendationService.downvote(1)
+
+      expect(recommendationRepository.find).toBeCalledTimes(1)
+      expect(recommendationRepository.updateScore).toBeCalledTimes(1)
+      expect(recommendationRepository.remove).not.toBeCalled()
+    });
+
+  });
+});
 
 afterAll(async () => {
   await prisma.$disconnect();
